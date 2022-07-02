23bf903976e888b1893f893ce9587f08cf6af503
tikoot <tiko.otarashvili99@gmail.com>
2022-07-02 14:14:29 +0200

    add focus method to remove text inside input

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,20 @@
 document.getElementById("start").onclick = function () {
     location.href = "signup.html";
 };
+
+
+function removeText(x){
+    document.getElementById(x).style.display = 'none';
+}
+
+/*
+const form = document.getElementById('input_form');
+
+const username = document.getElementById('name');
+const email = document.getElementById('email');
+const number = document.getElementById('number');
+const date = document.getElementById('date');
+
+form.addEventListener('submit',(e) => {
+
+})*/
